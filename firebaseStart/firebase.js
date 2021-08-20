9ee661e99302800e00768a3ab4378c1ccfd0535a
ecrseer <juustino@live.com>
2021-08-20 20:47:22 -0300

    firebase conta certa

--- firebaseStart/firebase.js
+++ firebaseStart/firebase.js
@@ -1,7 +1,7 @@
 
 
         // Your web app's Firebase configuration
-        var firebaseConfig = {
+        var Old_firebaseConfig = {
         apiKey: "AIzaSyBykYZEZY8Bpd1zoL9djvSdH07ZS9HUa-s",
         authDomain: "projbloco30.firebaseapp.com",
         projectId: "projbloco30",
@@ -9,6 +9,23 @@
         messagingSenderId: "667102800271",
         appId: "1:667102800271:web:e428b09931a3e0c9e043fa"
     };
+    
+    var firebaseConfig = {
+
+        apiKey: "AIzaSyDQYcaY9B89obl52Opf8ORFl_6ywN9EbIg",
+    
+        authDomain: "libfyzin.firebaseapp.com",
+    
+        projectId: "libfyzin",
+    
+        storageBucket: "libfyzin.appspot.com",
+    
+        messagingSenderId: "480452003717",
+    
+        appId: "1:480452003717:web:9e402ed32c685f523526ec"
+    
+      };
+    
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
 
