efbdd04038ef774209cece85b0b5ce951295ea46
ecrseer <juustino@live.com>
2021-08-20 21:57:20 -0300

    navbar compono

---
+++ libfyvue/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div class="navbb">
+    <div class="sidenav">
+    <a href="index.html" class="botao">
+        <span class="fa fa-home"></span>
+        <p>Home</p>
+    </a>
+    <a href="index.html" class="botao">
+        <span class="fa fa-home"></span>
+        <p>Playlist</p>
+    </a>
+    <a href="index.html" class="botao">
+        <span class="fa fa-home"></span>
+        <p>Home</p>
+    </a>
+    <h6>Your Library</h6>
+    <p>Favoritas</p>
+    <p>Favoritas</p>
+</div>
+<section class="topofixo">
+    <input type="text"
+           placeholder="Artists,songs or podcasts" class="pesquisa">
+
+    <a href="signup.html" class="btn">Sign up</a>
+    <a href="login.html" class="btn">Log in</a>
+</section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ @import 'index.css';
+</style>
