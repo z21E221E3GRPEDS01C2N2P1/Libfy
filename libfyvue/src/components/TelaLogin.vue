059e514cd551be302f9bf5d9107867682374b185
ecrseer <juustino@live.com>
2021-08-21 01:14:09 -0300

    logica de negocio

--- libfyvue/src/components/TelaLogin.vue
+++ libfyvue/src/components/TelaLogin.vue
@@ -65,8 +65,53 @@ export default {
   name: 'TelaLogin',
   props: {
     msg: String
-  }
+  },
+  data:{
+        email:'',
+        senha:'',
+        msgErro:'',
+        vers:'nome musica elvis',
+        logando:true,
+        firestore:null
+
+    },
+    methods:{
+        cadastrarLogin(e){
+            e.preventDefault()
+            firebase.auth()
+                .createUserWithEmailAndPassword(
+                this.email,
+                this.senha)
+                .then((credenciais)=>{
+                    let usuario = credenciais.user;
+                    console.log(usuario)
+                })
+                .catch((erro)=>{
+                    this.msgErro=`${erro.code} ${erro.message}`
+                })
+        },
+        logarLogin(e){
+            firebase.auth().
+                signInWithEmailAndPassword()
+        },
+
+    },
+    mounted(){
+        function initFireStore(){
+            firestore = firebase.firestore()
+        }
+        initFireStore()
+        const  collection = firestore.collection("musicas")
+        collection.add({
+            nome:"Elvis",
+            tocou:234.34,
+            cod:"230485"
+        })
+    }
+
 }
+ 
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
