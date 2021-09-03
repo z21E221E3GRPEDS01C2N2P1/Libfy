e785b655333fbd9fbaad21cea3732c8f7870035b
ecrseer <juustino@live.com>
2021-09-03 18:33:49 -0300

    firebase import all components

--- libfyvue/src/components/TelaLogin.vue
+++ libfyvue/src/components/TelaLogin.vue
@@ -96,7 +96,7 @@
 </template>
 
 <script>
-import firebase from "firebase";
+import {firebase} from '../const'
 
 export default {
   name: "TelaLogin",
@@ -128,29 +128,14 @@ export default {
         });
     },
     logarLogin(e) {
-      firebase.auth().signInWithEmailAndPassword();
+      firebase.auth().signInWithEmailAndPassword(
+        this.emailu,this.senha).then(
+
+        );
     },
   },
   mounted() {
-    function importFire() {
-      var firebaseConfig = {
-        apiKey: "AIzaSyDQYcaY9B89obl52Opf8ORFl_6ywN9EbIg",
-
-        authDomain: "libfyzin.firebaseapp.com",
-
-        projectId: "libfyzin",
-
-        storageBucket: "libfyzin.appspot.com",
-
-        messagingSenderId: "480452003717",
-
-        appId: "1:480452003717:web:9e402ed32c685f523526ec",
-      };
-
-      // Initialize Firebase
-      firebase.initializeApp(firebaseConfig);
-    }
-    importFire();
+    
 
     /* function initFireStore() {
       this.firestore = firebase.firestore();
--- libfyvue/src/const.js
+++ libfyvue/src/const.js
@@ -1,5 +1,26 @@
 import Home from './views/Home.vue'
 import apiD_musicas from './assets/apiDev_musicas.json'
+import firebase from "firebase";
+function importFire() {
+  var firebaseConfig = {
+    apiKey: "AIzaSyDQYcaY9B89obl52Opf8ORFl_6ywN9EbIg",
+
+    authDomain: "libfyzin.firebaseapp.com",
+
+    projectId: "libfyzin",
+
+    storageBucket: "libfyzin.appspot.com",
+
+    messagingSenderId: "480452003717",
+
+    appId: "1:480452003717:web:9e402ed32c685f523526ec",
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+importFire();
+
 const rotasmain = [
   {
     path: '/',
@@ -28,4 +49,4 @@ const rotasmain = [
   ]
 
 
-  export {rotasmain,apiD_musicas};
+  export {rotasmain, apiD_musicas, firebase};
