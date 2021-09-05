79d09a360e2657945a1d6428a8b11d010d8c3b80
ecrseer <juustino@live.com>
2021-09-05 01:00:22 -0300

    bypass fail: cors

--- libfyvue/src/store.js
+++ libfyvue/src/store.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 import createPersistedState from 'vuex-persistedstate'
-import { LIBFY_CLIENT_ID, LIBFY_CLIENT_SECRET, LIBFY_REFRESHH_TOKEN, post_GETTOKENURL } from './const'
+import { LIBFY_CLIENT_ID, LIBFY_CLIENT_SECRET, LIBFY_FIRST_ACCESSTOKEN, LIBFY_REFRESHH_TOKEN, post_GETTOKENURL } from './const'
 import { apiD_musicas } from "./const";
 
 Vue.use(Vuex)
@@ -20,7 +20,8 @@ const store = new Vuex.Store({
         loggedIn: false
       },
       maisTocadasArray: [],
-      token_acesso:''
+      libfy_token_acesso: '',
+      libfy_novo_refresh: ''
     }
 
   },
@@ -35,7 +36,7 @@ const store = new Vuex.Store({
       }
       return state.user.data.email ? state.user.data.email : '';
     },
-    getMaisTocadasArr(state){return state.maisTocadasArray},
+    getMaisTocadasArr(state) { return state.maisTocadasArray },
     getQualquerCois(state) {
       return state.qualquerCois;
     }
@@ -51,26 +52,33 @@ const store = new Vuex.Store({
     SET_MUSICAS_MAIS_TOCADAS(state, data) {
       let { albums } = data
       state.maisTocadasArray = albums
-    }, 
-    SET_TOKENACESS(state,pload){
-      state.token_acesso=pload
-    }
+    },
+    SET_LIBFY_TOKENACESS(state, { access, refresh }) {
+      state.libfy_token_acesso = access
+      state.libfy_novo_refresh = refresh
+    },
 
   },
   actions: { // equivalente ao methods de um componente
-    async carregar({ commit }) {
-      commit('SET_MUSICAS_MAIS_TOCADAS',apiD_musicas.amostra_dados_spotify_famosos)
+    async carregar({ commit, state }) {
+      if (state.libfy_novo_refresh === '') {
+        commit('SET_LIBFY_TOKENACESS', {
+          access: LIBFY_FIRST_ACCESSTOKEN,
+          refresh: LIBFY_REFRESHH_TOKEN
+        })
+      }
+      commit('SET_MUSICAS_MAIS_TOCADAS', apiD_musicas.amostra_dados_spotify_famosos)
       console.log('carregou')
 
       const config_get_axios = {
         headers: {
-          Authorization: `Bearer ${LIBFY_REFRESHH_TOKEN}`,
+          Authorization: `Bearer ${state.libfy_novo_refresh}`,
 
           Accept: 'application/json',
           'Content-Type': 'application/x-www-form-urlencoded',
         }
       };
-      const config_refreshtoken= {
+      const config_refreshtoken = {
         headers: {
           Authorization: `Basic ${LIBFY_CLIENT_ID} : ${LIBFY_CLIENT_SECRET}`,
 
@@ -78,17 +86,30 @@ const store = new Vuex.Store({
           'Content-Type': 'application/x-www-form-urlencoded',
         }
       }
-      
+
       axios.get("https://api.spotify.com/v1/browse/" +
         "new-releases?country=SE&limit=10&offset=5",
         config_get_axios).then(({ data }) => {
           console.log(data)
-          commit('SET_MUSICAS_MAIS_TOCADAS',data.albums.items)
+          commit('SET_MUSICAS_MAIS_TOCADAS', data.albums.items)
         }
-        ).catch((meLasquei)=>{
-          axios.post(post_GETTOKENURL)
+        ).catch((meLasquei) => {
+          let url_gettoken = post_GETTOKENURL;
+          url_gettoken += "grant_type=refresh_token";
+          url_gettoken += "&refresh_token=" + LIBFY_REFRESHH_TOKEN;
+          url_gettoken += "&client_id=" + LIBFY_CLIENT_ID;
+
+          console.log('vou postar no' + url_gettoken)
+
+          axios.post(url_gettoken, config_refreshtoken).then(data => {
+            console.log('meLasquei, data eh' + data + 'meLasquei')
+            commit('SET_LIBFY_TOKENACESS', {
+              access: data.access_token,
+              refresh: data.refresh_token
+            })
+          }).catch(lascouMuito => console.log('lascoumt' + lascouMuito))
         })
-    }, 
+    },
     async carregarUsuario({ commit }, user) {
       commit("SET_LOGGED_IN", user !== null);
       if (user) {
@@ -103,7 +124,7 @@ const store = new Vuex.Store({
     async logaUsuario({ commit }, cred) {
       console.log('ave')
     },
-    
+
 
   },
   plugins: [meuDataStore]
