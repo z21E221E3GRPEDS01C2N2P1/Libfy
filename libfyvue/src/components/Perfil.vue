60aa86025683ab3b02301cdd48f9705d68e825e3
Elvis Lopes <evs.lopes@gmail.com>
2021-09-17 16:33:13 -0300

    Ajuste na exibição do perfil

--- libfyvue/src/components/Perfil.vue
+++ libfyvue/src/components/Perfil.vue
@@ -1,43 +1,43 @@
 <template>
-  <div class="home"> 
+  <div class="home">
     <v-container>
       <v-row>
         <div class="flex-column">
-          <i class="fas fa-user grande" color="white"></i>
+          <i class="fas fa-user grande text-center py-6" color="white"></i>
           {{ nomeUsuario ? nomeUsuario : '' }}
         </div>
       </v-row>
 
       <v-btn
-        fixed
-        right
-        bottom
-        rounded
-        dark
-        large
-        color="purple"
-        class="mx-2 botaomobile"
-        v-on:click="deslogar"
+          fixed
+          right
+          bottom
+          rounded
+          dark
+          large
+          color="purple"
+          class="mx-2 botaomobile"
+          v-on:click="deslogar"
       >
-        <v-icon dark> Log out </v-icon>
+        <v-icon dark> Log out</v-icon>
       </v-btn>
 
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolor
-        aperiam hi </h1>
+      <h1 class="text-lg-h6 text-center py-6">
+        Está tela contará com as informações do usuário futuramente implementadas
+      </h1>
     </v-container>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex"; 
+import {mapGetters} from "vuex";
 // @ is an alias to /src 
 
 export default {
   name: "Perfil",
   computed: {
-    ...mapGetters(["getUser","getNomeUsuario"]),
-     
+    ...mapGetters(["getUser", "getNomeUsuario"]),
+
     nomeUsuario() {
       return this.getNomeUsuario || ''
     },
@@ -46,21 +46,22 @@ export default {
   methods: {
     deslogar() {
       this.$firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          this.$router.replace({
-            name: "Home",
+          .auth()
+          .signOut()
+          .then(() => {
+            this.$router.replace({
+              name: "Home",
+            });
           });
-        });
     },
-  }, 
+  },
 };
 </script>
 <style scoped>
 .grande {
   font-size: 22vh;
 }
+
 .botaomobile {
 }
 </style>
